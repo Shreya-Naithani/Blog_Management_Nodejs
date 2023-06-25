397f4dd7ebecb0f66d9740ffa477ffa1a96924f7
Shreya-Naithani <102851285+Shreya-Naithani@users.noreply.github.com>
2023-06-25 10:59:05 +0530

    Update connection.js

--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ const mongoose =require('mongoose');
 const connectDB=async()=>{
     try{
 // mongodb connection string
-  const con =await mongoose.connect("mongodb://127.0.0.1:27017/BLOG_PROJECT",{
+  const con =await mongoose.connect("mongodb+srv://hello:hello1@blogmanagement.siz0546.mongodb.net/",{
     useNewUrlParser:true,
   })
   console.log(`mongodb connected:${con.connection.host}`);
